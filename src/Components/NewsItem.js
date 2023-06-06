c623587c2c591888c6eea39456e76b0085247e0b
Farooq haider alvi <farooqhaideralvi1999@gmail.com>
2023-06-06 18:57:02 +0500

    fetching data from newsapi and making next prev btn

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -4,16 +4,15 @@ export default class NewsItem extends Component {
 
 
     render() {
-        let { description, url, imageUrl,title,ind } = this.props;
+        let { description, url, imageUrl,title } = this.props;
         return (
 
-            
                 <div className="card" style={{ width: "18rem" }}>
                     <img src={imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={url} target='_blannk' className="btn btn-primary">View Detials</a>
+                        <a href={url} target='_blannk' className="btn btn-outline-dark">View Detials</a>
                     </div>
                 </div>
             
